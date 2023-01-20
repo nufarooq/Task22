8b3722b24e54df07b9082116d5f26255b2b57da0
Tejora <numan.992@gmail.com>
2023-01-20 21:44:43 +0530

    Task commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task22_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetUniqueNumbers(string Number)
+        {
+            string uniqueNumber = "";
+            for (int i = 0; i < Number.Length; i++)
+            {
+                if (uniqueNumber.Contains(Number[i].ToString()))
+                {
+                    continue;
+                }
+                uniqueNumber += Number[i];
+            }
+            
+            return Json(uniqueNumber, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task22_MVC.MvcApplication" Language="C#" %>
